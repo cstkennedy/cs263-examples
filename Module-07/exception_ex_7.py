cb69882222fd7db3f201dfeede31eb49bf6b7b55
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2023-11-28 13:10:41 -0500

    Continue second exception lecture

---
+++ Module-07/exception_ex_7.py
@@ -0,0 +1,60 @@
+from typing import TextIO, Generator
+import sys
+
+
+def parse_shape_file(input_file: TextIO) -> Generator[tuple[str, list[float]], None, None]:
+    """
+    Take each line from a given file (or file-like object) and split it into a
+    tuple in the form
+
+      (name, [val_1, val_2, ...])
+
+    If a line is invalid (e.g., contains non-numeric values after the
+    semicolon)... skip the line.
+    """
+
+    for line in input_file:
+        # Remove leading and trailing whitespace
+        line = line.strip()
+
+        if not line:
+            continue
+
+        try:
+            name, the_rest = line.split(";")
+        except ValueError as _err:
+            print(f"Missing ';' -> \"{line}\" is malformed.", file=sys.stderr)
+            continue
+
+        the_rest = the_rest.lstrip()
+        the_rest = the_rest.split()
+
+        try:
+            numbers = [float(val) for val in the_rest]
+        except ValueError as err:
+            print(f"{err} -> \"{line}\" is malformed.", file=sys.stderr)
+            continue
+
+        yield (name, numbers)
+
+
+def main():
+
+    try:
+        shape_filename = sys.argv[1]
+
+    except IndexError as _err:
+        print("Usage: exception_ex_5.py INPUT_FILENAME")
+        sys.exit(1)
+
+    try:
+        with open(shape_filename, "r") as shape_file:
+            shape_data = list(parse_shape_file(shape_file))
+
+    except FileNotFoundError as err:
+        print(err)
+        sys.exit(2)
+
+
+if __name__ == "__main__":
+    main()
