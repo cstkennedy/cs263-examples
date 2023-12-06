2e1281d228ef22d47122e240b4ba94eb6b149f99
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2023-12-05 20:55:51 -0500

    Start Refactoring TDD lecture

--- Module-08/Painting-3-Tests/tests/test_compute_paint.py
+++ Module-08/Painting-3-Tests/tests/test_compute_paint.py
@@ -5,14 +5,6 @@ from hamcrest import *
 from compute_paint import (wall_surface_area, gallons_required)
 
 
-def test_wall_surface_area():
-    assert_that(wall_surface_area(1, 1), equal_to(4))
-    assert_that(wall_surface_area(1, 2), equal_to(8))
-    assert_that(wall_surface_area(2, 2), equal_to(16))
-
-    assert_that(wall_surface_area(10, 12), close_to(480, 1e-1))
-    assert_that(wall_surface_area(10.5, 10), close_to(420, 1e-1))
-
 test_data = [
     (1, 1, 4),
     (1, 2, 8),
@@ -22,31 +14,14 @@ test_data = [
 ]
 
 @pytest.mark.parametrize("length, width, surface_area", test_data)
-def test_wall_surface_area_p(length, width, surface_area):
+def test_wall_surface_area(length, width, surface_area):
     assert_that(wall_surface_area(length, width), close_to(surface_area, 1e-1))
 
 
 def test_gallons_required():
-    lower, upper = gallons_required(wall_area=1)
-    assert_that(lower, equal_to(1))
-    assert_that(upper, equal_to(1))
-
-    lower, upper = gallons_required(wall_area=10)
-    assert_that(lower, equal_to(1))
-    assert_that(upper, equal_to(1))
-
-    lower, upper = gallons_required(wall_area=100)
-    assert_that(lower, equal_to(1))
-    assert_that(upper, equal_to(1))
-
-    lower, upper = gallons_required(wall_area=1_000)
-    assert_that(lower, equal_to(3))
-    assert_that(upper, equal_to(3))
-
-    lower, upper = gallons_required(wall_area=1_050)
-    assert_that(lower, equal_to(3))
-    assert_that(upper, equal_to(3))
-
-    lower, upper = gallons_required(wall_area=1200)
-    assert_that(lower, equal_to(3))
-    assert_that(upper, equal_to(4))
+    assert_that(gallons_required(wall_area=1), equal_to((1, 1)))
+    assert_that(gallons_required(wall_area=10), equal_to((1, 1)))
+    assert_that(gallons_required(wall_area=100), equal_to((1, 1)))
+    assert_that(gallons_required(wall_area=1_000), equal_to((3, 3)))
+    assert_that(gallons_required(wall_area=1_050), equal_to((3, 3)))
+    assert_that(gallons_required(wall_area=1_200), equal_to((3, 4)))
